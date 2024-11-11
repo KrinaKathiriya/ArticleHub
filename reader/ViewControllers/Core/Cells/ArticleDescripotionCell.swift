f981420c0da074ab35530afb087af32c72aa5f2e
Krina <krina.pixster@gmail.com>
2024-11-11 18:18:49 +0530

    Updated HTML Response setup

---
+++ reader/ViewControllers/Core/Cells/ArticleDescripotionCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleDescripotionCell.swift
+//  reader
+//
+//  Created by Krina on 11/11/24.
+//
+
+import UIKit
+
+class ArticleDescripotionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
